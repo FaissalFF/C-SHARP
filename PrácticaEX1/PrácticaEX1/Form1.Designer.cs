1c6ff5a680a55c4f6a57081f674761c8dcb037f1
FaissalFF <146260353+FaissalFF@users.noreply.github.com>
2023-11-23 15:49:47 +0100

    Add files via upload

---
+++ PrácticaEX1/PrácticaEX1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace PrácticaEX1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_generar = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbl_cantidad1 = new System.Windows.Forms.Label();
+            this.lbl_cantidad2 = new System.Windows.Forms.Label();
+            this.lbl_cantidad3 = new System.Windows.Forms.Label();
+            this.lbl_cantidad4 = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_generar
+            // 
+            this.btn_generar.Location = new System.Drawing.Point(13, 13);
+            this.btn_generar.Name = "btn_generar";
+            this.btn_generar.Size = new System.Drawing.Size(136, 34);
+            this.btn_generar.TabIndex = 0;
+            this.btn_generar.Text = "Generar";
+            this.btn_generar.UseVisualStyleBackColor = true;
+            this.btn_generar.Click += new System.EventHandler(this.btn_generar_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 84);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(855, 150);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 240);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(855, 156);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(13, 402);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(855, 166);
+            this.flowLayoutPanel3.TabIndex = 2;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(12, 575);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(855, 166);
+            this.flowLayoutPanel4.TabIndex = 3;
+            // 
+            // lbl_cantidad1
+            // 
+            this.lbl_cantidad1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cantidad1.Location = new System.Drawing.Point(883, 131);
+            this.lbl_cantidad1.Name = "lbl_cantidad1";
+            this.lbl_cantidad1.Size = new System.Drawing.Size(73, 49);
+            this.lbl_cantidad1.TabIndex = 4;
+            // 
+            // lbl_cantidad2
+            // 
+            this.lbl_cantidad2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cantidad2.Location = new System.Drawing.Point(883, 294);
+            this.lbl_cantidad2.Name = "lbl_cantidad2";
+            this.lbl_cantidad2.Size = new System.Drawing.Size(73, 49);
+            this.lbl_cantidad2.TabIndex = 5;
+            // 
+            // lbl_cantidad3
+            // 
+            this.lbl_cantidad3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cantidad3.Location = new System.Drawing.Point(883, 456);
+            this.lbl_cantidad3.Name = "lbl_cantidad3";
+            this.lbl_cantidad3.Size = new System.Drawing.Size(73, 49);
+            this.lbl_cantidad3.TabIndex = 6;
+            // 
+            // lbl_cantidad4
+            // 
+            this.lbl_cantidad4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cantidad4.Location = new System.Drawing.Point(883, 611);
+            this.lbl_cantidad4.Name = "lbl_cantidad4";
+            this.lbl_cantidad4.Size = new System.Drawing.Size(73, 49);
+            this.lbl_cantidad4.TabIndex = 7;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(201, 13);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(127, 33);
+            this.btn_salir.TabIndex = 8;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(489, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(406, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cantidad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 753);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.lbl_cantidad4);
+            this.Controls.Add(this.lbl_cantidad3);
+            this.Controls.Add(this.lbl_cantidad2);
+            this.Controls.Add(this.lbl_cantidad1);
+            this.Controls.Add(this.flowLayoutPanel4);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btn_generar);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_generar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.Label lbl_cantidad1;
+        private System.Windows.Forms.Label lbl_cantidad2;
+        private System.Windows.Forms.Label lbl_cantidad3;
+        private System.Windows.Forms.Label lbl_cantidad4;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PrácticaEX1/PrácticaEX1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrácticaEX1
+{
+    public partial class Form1 : Form
+    {
+        Random r1 = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_generar_Click(object sender, EventArgs e)
+        {
+            int cont1 = 0;
+            int cont2 = 0;
+            int cont3 = 0;
+            int cont4 = 0;
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel2.Controls.Clear();
+            flowLayoutPanel3.Controls.Clear();
+            flowLayoutPanel4.Controls.Clear();
+            if (int.Parse(textBox1.Text) > 0 && int.Parse(textBox1.Text) < 101) {
+
+                for (int i = 0; i < int.Parse(textBox1.Text); i++)
+                {
+                    int num_random = r1.Next(1, 100);
+                    if (num_random > 0 && num_random < 26)
+                    {
+                        cont1++;
+                        Label lbl1 = new Label();
+                        lbl1.Text = Convert.ToString(num_random);
+                        lbl1.ForeColor = Color.Blue;
+                        flowLayoutPanel1.Controls.Add(lbl1);
+
+                    } else if (num_random > 25 && num_random < 51)
+                    {
+                        cont2++;
+                        Label lbl2 = new Label();
+                        lbl2.Text = Convert.ToString(num_random);
+                        lbl2.ForeColor = Color.Green;
+                        flowLayoutPanel2.Controls.Add(lbl2);
+                    } else if (num_random > 50 && num_random < 76)
+                    {
+                        cont3++;
+                        Label lbl3 = new Label();
+                        lbl3.Text = Convert.ToString(num_random);
+                        lbl3.ForeColor = Color.Orange;
+                        flowLayoutPanel3.Controls.Add(lbl3);
+                    }
+                    else if (num_random > 75 && num_random < 101)
+                    {
+                        cont4++;
+                        Label lbl4 = new Label();
+                        lbl4.Text = Convert.ToString(num_random);
+                        lbl4.ForeColor = Color.Red;
+                        flowLayoutPanel4.Controls.Add(lbl4);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, debe colocar un número entre 0 y 100", "Info", MessageBoxButtons.OK);
+            }
+            lbl_cantidad1.Text = Convert.ToString(cont1);
+            lbl_cantidad2.Text = Convert.ToString(cont2);
+            lbl_cantidad3.Text = Convert.ToString(cont3);
+            lbl_cantidad4.Text = Convert.ToString(cont4);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                DialogResult resultado = MessageBox.Show("Seguro que quieres salir del programa?", "Info", MessageBoxButtons.OKCancel);
+                if (resultado == DialogResult.OK)
+                {
+                    this.Close();
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
